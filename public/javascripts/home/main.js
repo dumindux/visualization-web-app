eeb9223b0e2b121decba461896fc591599b5faa2
nuwanprabhath <nuwanprabhath@gmail.com>
2016-01-12 09:42:51 +0530

    Moved JS slideshow init part to main js file

--- public/javascripts/home/main.js
+++ public/javascripts/home/main.js
@@ -127,6 +127,26 @@ function main() {
 
     });
 
+    /**
+     * Picture slide show
+     */
+
+    var opts = {
+        auto: {
+            speed: 3500,
+            pauseOnHover: true
+        },
+        fullScreen: false,
+        swipe: true
+    };
+    makeBSS('.num1', opts);
+    var opts2 = {
+        auto: false,
+        fullScreen: true,
+        swipe: true
+    };
+    makeBSS('.num2', opts2);
+
   	/*====================================
     WOW JS
     ======================================*/	
