e0be053f37453cbb52c9126945a2e1781e9d5363
Dumindu Buddhika <dumindukarunathilaka@gmail.com>
2016-01-17 09:58:01 +0530

    finished average google map

--- public/javascripts/averageGMap/renderer.js
+++ public/javascripts/averageGMap/renderer.js
@@ -1,24 +1,41 @@
 var map;
 jsondata = JSON.parse(jsondata);
+var circles = [];
+var points = [];
 
 function initMap() {
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 6.8, lng: 80 },
     zoom:10,
-    styles: mapStyle
   });
 
 //  map.data.setStyle(styleFeature);
 
+  select = document.getElementById('combobox');
+  for(var i = 0; i < jsondata.length; i++) {
+    var opt = document.createElement('option');
+    opt.value = i;
+    opt.innerHTML = jsondata[i][0];
+    select.appendChild(opt);
+  }
+
+  addMarkers(0);
+}
+
+function changeMarkers(event) {
+  addMarkers(this.selectedIndex);
+}
+
+function addMarkers(index) {
+  removeMarkers();
   var low = [151, 83, 34];   // color of mag 1.0
   var high = [5, 69, 54];  // color of mag 6.0 and above
   var minMag = 1.0;
   var maxMag = 1000;
 
-
-
-  for(var i = 0; i < jsondata[0][1].length; i++) {
-    var fraction = (Math.min(jsondata[0][1][3], maxMag) - minMag) /
+  for (var i = 0; i < jsondata[0][1].length; i++) {
+    var fraction = (Math.min(jsondata[index][1][3], maxMag) - minMag) /
         (maxMag - minMag);
     var color = interpolateHsl(low, high, fraction);
 
@@ -26,20 +43,31 @@ function initMap() {
       strokeColor: '#fff',
       strokeWeight: 0.5,
       fillColor: color,
-      fillOpacity: 30 / jsondata[0][1][3] ,
+      fillOpacity: 30 / jsondata[index][1][3] ,
+      animation: google.maps.Animation.DROP,
       map: map,
-      center: new google.maps.LatLng(jsondata[0][1][1], jsondata[0][1][2]),
-      radius: jsondata[0][1][3] * 100
+      center: new google.maps.LatLng(jsondata[index][1][1], jsondata[index][1][2]),
+      radius: jsondata[index][1][3] * 100
     });
-  }
+    circles.push(cityCircle);
 
-  // Get the earthquake data (JSONP format)
-  // This feed is a copy from the USGS feed, you can find the originals here:
-  //   http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-  //var script = document.createElement('script');
-  //script.setAttribute('src',
-  //  'https://storage.googleapis.com/maps-devrel/quakes.geo.json');
-  //document.getElementsByTagName('head')[0].appendChild(script);
+    var point = new google.maps.Marker({
+      position: new google.maps.LatLng(jsondata[index][1][1], jsondata[index][1][2]),
+      label: jsondata[index][1][0],
+      animation: google.maps.Animation.DROP,
+      map: map
+    });
+    points.push(point);
+  }
+}
+
+function removeMarkers() {
+  for (var i = 0; i < circles.length; i++) {
+    circles[i].setMap(null);
+    points[i].setMap(null);
+  }
+  circles = [];
+  points = [];
 }
 
 function interpolateHsl(lowHsl, highHsl, fraction) {
